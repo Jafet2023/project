8ce6659012c5196c3f0f77cbe6258e8989e560bd
jafet2023 <jafet_1997@hotmail.es>
2019-07-21 21:49:55 -0400

    update reclamo

--- src/app/pages/reclamo/reclamo.page.ts
+++ src/app/pages/reclamo/reclamo.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProvidersService } from 'src/app/providers.service';
+import { IonicSelectableComponent } from 'ionic-selectable';
 
 
 @Component({
@@ -7,21 +8,43 @@ import { ProvidersService } from 'src/app/providers.service';
   templateUrl: './reclamo.page.html',
   styleUrls: ['./reclamo.page.scss'],
 })
-export class ReclamoPage implements OnInit {
 
-  empresas: any;
+export class ReclamoPage implements OnInit {
+  Empresas: any[];
+  // Empresa: empresasel;
+  empresas : any;
+  item: { id: any; nombre: any; };
+  // item: empresasel;
+
   
-  constructor(private serviceProvider: ProvidersService) { }
+  constructor(private serviceProvider: ProvidersService) {
+    this.Empresas = [
+    ];
+  }
 
   ngOnInit() {
-    // this.serviceProvider.getEmpresas().then(data => {
-    //   this.empresas = data;
-    //   console.log(this.empresas);
-    // })
     this.serviceProvider.getEmpresas().then( data => {
-        this.empresas = data;
-        console.log(this.empresas);
-      });
+      this.empresas = data;
+      console.log(this.empresas);
+      for(let i=0;i<this.empresas.length;i++){
+          this.item = {
+              id: this.empresas[i].id,
+              nombre: this.empresas[i].nombre
+          };
+          this.Empresas.push(this.item);
+      }
+    });
+  }
+
+  portChange(event: {
+    component: IonicSelectableComponent,
+    value: any 
+  }) {
+    console.log('port:', event.value);
   }
 
 }
+// class empresasel{
+//   public id: number;
+//   public nombre: string;
+// }
