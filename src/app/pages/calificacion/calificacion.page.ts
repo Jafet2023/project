2166aeb59077fa7ab49b31cb7352c094e3ce330e
jafet2023 <jafet_1997@hotmail.es>
2019-08-12 21:54:07 -0400

    califiacion

--- src/app/pages/calificacion/calificacion.page.ts
+++ src/app/pages/calificacion/calificacion.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProvidersService } from 'src/app/providers.service';
+import { IonicSelectableComponent } from 'ionic-selectable';
 
 @Component({
   selector: 'app-calificacion',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalificacionPage implements OnInit {
 
-  constructor() { }
+  Empresas: any[];
+  // Empresa: empresasel;
+  empresas : any;
+  
+  Reclamos: any[];
+  reclamos: any;
+  item: { _id: any; nombre: any; };
+
+  constructor(private serviceProvider: ProvidersService) { 
+    this.Empresas = [];
+  }
 
   ngOnInit() {
+    this.serviceProvider.getEmpresas().then( data => {
+      this.empresas = data;
+      console.log(this.empresas);
+      for(let i=0;i<this.empresas.length;i++){
+          this.item = {
+              _id: this.empresas[i]._id,
+              nombre: this.empresas[i].nombre
+          };
+          this.Empresas.push(this.item);
+      }
+    });
+  }
+
+  portChange(event: {
+    component: IonicSelectableComponent,
+    value: any 
+  }) {
+    console.log('port:', event.value);
+  }
+
+  PostReclamo(fRegistro: NgForm) {
+    this.serviceProvider.PostReclamo(fRegistro.value.descripcion,fRegistro.value.empresa)
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
